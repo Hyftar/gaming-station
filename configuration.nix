54d4fd0b24b303c4c2a1d710999760dec292847a
Simon Landry <simon.landry762@gmail.com>
2026-08-29 13:34:13 -0400

    Add hardware-configuration; Fix several configs

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,42 @@
 { config, pkgs, lib, ... }:
 
 {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  networking.hostName = "kalista";
+
+  users.users."hyftar" = {
+    isNormalUser = true;
+    description = "Simon Landry";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  time.timeZone = "America/Toronto";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  networking.networkmanager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
   services.xserver.enable = true;
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "breeze";
+    theme = "breeze-dark";
   };
 
   services.desktopManager.plasma6.enable = true;
+  services.printing.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
@@ -28,10 +55,18 @@
 
   services.xserver.xkb.options = "compose:ralt";
 
+  services.openssh.enable = true;
+
+  programs.firefox.enable = true;
+
   environment.systemPackages = with pkgs; [
-    firefox
     kdePackages.ark
     kdePackages.spectacle
+    kdePackages.kate
+    vim
+    wget
+    curl
+    git
   ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
@@ -47,7 +82,31 @@
 
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-emoji
+    noto-fonts-color-emoji
     liberation_ttf
   ];
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
 }
---
+++ hardware-configuration.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/6ad346eb-5334-46aa-9cf8-0a0b3d3295a7";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [
+      {
+        device = "/dev/disk/by-uuid/bb547cd7-4c91-4836-ba89-33a3f08d27f5";
+      }
+    ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
